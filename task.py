bc7b72150fb575f9d0d62acc607f6df2a49df6f9
Dilshada101 <dilshadafarooq1014@gmail.com>
2026-05-12 12:10:04 +0530

    Task completed

---
+++ task.py
@@ -0,0 +1,7 @@
+import re 
+with open ("file.log", 'r') as f:
+   content=f.read()
+   x=re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', content)
+   print(set(x))
+with open ("result.log", 'w') as f:
+   content=f.write(str(set(x)))
